84eced476d944ee3e80a2ac848ea37d9509cbb16
Felipe <felipeas314@gmail.com>
2020-07-10 07:22:22 -0300

    seperando em components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título"/>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro"  v-bind:key="foto.titulo">
+        <Painel :titulo="foto.titulo">
+          <ImageResponsiva :url="foto.url" :titulo="foto.titulo" />
+        </Painel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Painel from './components/shared/painel/Painel.vue';
+import ImageResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Painel,
+    ImageResponsiva
+  },
+  name: 'App',
+  data() {
+    return {
+      titulo: 'AluraPIC',
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+
+    fotosComFiltro() {
+
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+
+    display: inline-block;
+  }
+
+  .filtro {
+
+    display: block;
+    width: 100%;
+  }
 </style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="painel-titulo" @dbclick="visivel = !visivel">{{titulo}}</h1>
+    <transition name="painel-fade">
+      <div class="painel-conteudo" v-show="visivel">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['titulo'],
+
+  data() {
+    return {
+      visivel: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
